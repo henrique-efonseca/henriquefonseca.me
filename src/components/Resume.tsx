4604ac742083a4a6f94cbff6169fb8f3a15a7d0a
Henrique Fonseca <henrique@fonseca.email>
2024-10-06 00:36:16 +0100

    fix: resolve build error

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-import { Work } from '../data/lifeApi';
+import { Experience } from '../data/lifeApi';
 import { BriefcaseIcon } from './icons/BriefcaseIcon';
 
 export const Resume = () => {
@@ -11,7 +11,7 @@ export const Resume = () => {
         <span className="ml-3">My Experience</span>
       </h2>
       <ol className="mt-6 space-y-4">
-        {Work.map((role, roleIndex) => (
+        {Experience.map((role, roleIndex) => (
           <li key={roleIndex} className="flex gap-4">
             <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
               <Image
